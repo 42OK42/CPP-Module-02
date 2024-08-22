ffee9a151f86b7fc51d781e1975cedf9bb3a3183
Oliver Krahl <krahlolli@gmail.com>
2024-08-22 14:48:49 +0200

    done

--- ex02/includes/Fixed.hpp
+++ ex02/includes/Fixed.hpp
@@ -28,26 +28,26 @@ class Fixed
 		Fixed();
 		Fixed(const int int_value);
 		Fixed(const float float_value);
-		Fixed(const Fixed& other);
+		Fixed(const Fixed &other);
 
 		~Fixed();
 
-		Fixed& operator=(const Fixed& other);
+		Fixed &operator=(const Fixed &other);
 		
 		int getRawBits(void) const;
 		void setRawBits(int const raw);
 
-		bool operator>(const Fixed& other) const;
-		bool operator<(const Fixed& other) const;
-		bool operator>=(const Fixed& other) const;
-		bool operator<=(const Fixed& other) const;
-		bool operator==(const Fixed& other) const;
-		bool operator!=(const Fixed& other) const;
+		bool operator>(const Fixed &other) const;
+		bool operator<(const Fixed &other) const;
+		bool operator>=(const Fixed &other) const;
+		bool operator<=(const Fixed &other) const;
+		bool operator==(const Fixed &other) const;
+		bool operator!=(const Fixed &other) const;
 
-		Fixed operator+(const Fixed& other) const;
-		Fixed operator-(const Fixed& other) const;
-		Fixed operator*(const Fixed& other) const;
-		Fixed operator/(const Fixed& other) const;
+		Fixed operator+(const Fixed &other) const;
+		Fixed operator-(const Fixed &other) const;
+		Fixed operator*(const Fixed &other) const;
+		Fixed operator/(const Fixed &other) const;
 
 		Fixed& operator++();
 		Fixed operator++(int);
@@ -58,11 +58,11 @@ class Fixed
 		int toInt(void) const;
 
 		static	Fixed &min(Fixed &a, Fixed &b);
-		static const Fixed &min(const Fixed& a, const Fixed& b);
+		static const Fixed &min(const Fixed &a, const Fixed &b);
 		static	Fixed &max(Fixed &a, Fixed &b);
-		static const Fixed &max(const Fixed& a, const Fixed& b);
+		static const Fixed &max(const Fixed &a, const Fixed &b);
 };
 
-std::ostream& operator<<(std::ostream& os, const Fixed& obj);
+std::ostream &operator<<(std::ostream &os, const Fixed &obj);
 
 #endif
